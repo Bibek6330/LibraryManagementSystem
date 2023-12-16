c821eb85f14cb84e1674094c2bc001e6932e2c62
Bibek6330 <113578686+Bibek6330@users.noreply.github.com>
2023-12-16 18:13:49 +0530

    Update Library.java
    "Implemented the Library class to manage a collection of books within a library system.
    
    The class utilizes a List to store Book objects and provides methods to add books, display available books, and find a book by its ID. The 'addBook' method appends a book to the library's collection, while 'displayAvailableBooks' prints details of all available books. Additionally, the 'findBookById' method enables searching for a specific book by its ID, returning the book if found or null if not.
    
    This design allows for efficient book management and retrieval within the library, facilitating a seamless integration into a larger library management system."

---
+++ src/Library.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+public class Library {
+    private List<Book> books;
+
+    public Library() {
+        this.books = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public void displayAvailableBooks() {
+        System.out.println("Available Books:");
+        for (Book book : books) {
+            if (book.isAvailable()) {
+                System.out.println(book);
+            }
+        }
+    }
+
+    public Book findBookById(int bookId) {
+        for (Book book : books) {
+            if (book.getBookId() == bookId) {
+                return book;
+            }
+        }
+        return null;
+    }
+}
